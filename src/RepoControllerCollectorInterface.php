9766b7ca689c631166bc017d69f88b51da8de657
Marcelo Vani <marcellovani@yahoo.co.uk>
2018-10-04 11:05:00 +0100

    Created service tag collector

---
+++ src/RepoControllerCollectorInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\config_pr;
+
+use Drupal\Component\Assertion\Inspector;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Interface definition for ConfigPr Repo Controllers.
+ *
+ * @see \Drupal\config_pr
+ */
+interface RepoControllerCollectorInterface {
+  /**
+   * Todo: Describe
+   *
+   * @param array $tags
+   *
+   * @return mixed
+   */
+  public function invalidateTags(array $tags);
+
+  /**
+   *
+   * Todo: Describe
+   * @param RepoControllerCollectorInterface $repoCollector
+   *
+   * @return mixed
+   */
+  public function addRepoController(RepoControllerCollectorInterface $repoCollector);
+
+}
